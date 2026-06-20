7b91390da963c6f0c1786c5e4c7ed68d12fc961b
Taha-tec <tahairfan05@yahoo.com>
2026-06-20 22:44:55 +0500

    fixed 3

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import PrismaPackage from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const { PrismaClient } = PrismaPackage;
 const globalForPrisma = globalThis;
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
